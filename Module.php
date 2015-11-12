97207e8f8fbdbfa29682325ad40c3da498ba1ada
Alex <tasmaniski.com>
2015-11-12 19:41:47 +0100

    composer files

--- Module.php
+++ Module.php
@@ -1,30 +1,3 @@
 <?php
 
-namespace ParamsHelper;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
-class Module {
-	public function onBootstrap(MvcEvent $e) {
-		$eventManager = $e->getApplication()->getEventManager();
-		$moduleRouteListener = new ModuleRouteListener();
-		$moduleRouteListener->attach($eventManager);
-	}
-
-	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
-
-	}
-
-	public function getAutoloaderConfig() {
-		return array(
-			'Zend\Loader\StandardAutoloader' => array(
-				'namespaces' => array(
-					__NAMESPACE__=> __DIR__ . '/src/' . __NAMESPACE__,
-				),
-			),
-		);
-	}
-
-}
+require_once __DIR__ . '/src/ParamsHelper/Module.php';
